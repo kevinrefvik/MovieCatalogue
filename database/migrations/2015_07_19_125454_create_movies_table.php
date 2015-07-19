b1de626562bf2ebcdeb28e2df908aaa4e98b41ce
vagrant <vagrant@homestead>
2015-07-19 12:59:00 +0000

    Main migrations added

---
+++ database/migrations/2015_07_19_125454_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('secondary_title');
+            $table->integer('category_id');
+            $table->boolean('favourite');
+            $table->boolean('watched');
+            $table->string('format');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('movies');
+    }
+}
