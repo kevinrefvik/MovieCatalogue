cd2e23fe7a338d97345b2dc54621e11d6b148114
Kevin Refvik <kevin@bubble.dk>
2015-07-22 08:15:43 +0200

    Added master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>MovieCatalogue</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					@yield('content')
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
